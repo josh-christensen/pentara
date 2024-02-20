db5758904a86e4f93251707492353a19fe9e25e5
Josh Christensen <joshuarc13@gmail.com>
2024-02-19 19:55:25 -0700

    Update documentation. Scaffolding for read_dataset

--- R/read_dataset.R
+++ R/read_dataset.R
@@ -13,17 +13,26 @@
 #' @export
 
 read_dataset <- function(name, type, client, protocol) {
+  # TODO Check if the function is being run from the P: drive
+  # TODO Check that type is populated
+  # TODO Implement argument matching for type
+
   file_name <- paste0(name, ".sas7bdat")
 
-  # Branch between
+  # Branch between RStudio response and regular version
   if (Sys.getenv("RSTUDIO") == "1") {
+    # Get the current document path
     current_document_path <- rstudioapi::getActiveDocumentContext()$path
+    # Get the protocol root directory
     protocol_root <- stringr::str_extract(current_document_path, "([^/]*/){4}")
     protocol_root <- stringr::str_remove(protocol_root, "/$")
+    # Create the full path to the dataset
     full_path <- file.path(protocol_root, "Data", "Production", "ADaM", file_name)
     dataset <- haven::read_sas(full_path)
   } else {
     stop("Method for users not on Rstudio not yet implemented")
+    # TODO Check if client, protocol and type are all populated
+    # TODO make client searchable from protocol
   }
 
   return(dataset)
